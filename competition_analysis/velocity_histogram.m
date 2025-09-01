d8e80bc56bee2a144007d8da8c5d802ef53a0a39
LucasDudley <141531242+LucasDudley@users.noreply.github.com>
2025-09-01 13:57:52 -0700

    updated with enveloping plots

--- competition_analysis/velocity_histogram.m
+++ competition_analysis/velocity_histogram.m
@@ -3,19 +3,28 @@ s = settings;
 s.matlab.appearance.figure.GraphicsTheme.TemporaryValue= 'light'; % set figure background to light
 
 % load data
-all_data = load('2025_endurance_data.mat');
+all_data = load("G:\.shortcut-targets-by-id\1vCayBu0JWPEaCjSa5KhpGMqdHFvsMCFY\Senior_Design\Damper_Info\2025_endurance_data.mat");
 
 %% Motion ratios: shock travel / wheel travel
 f_mr = 1.03;
 r_mr = 1.16;
 
-% convert to wheel travel (inches if pots are inches)
-data.time = all_data.Shock_Pots_FL_Lin_Pot_Value.Time;
-data.FL_pos = all_data.Shock_Pots_FL_Lin_Pot_Value.Value / f_mr;
-data.FR_pos = all_data.Shock_Pots_FR_Lin_Pot_Value.Value / f_mr;
-data.RL_pos = all_data.Shock_Pots_RL_Lin_Pot_Value.Value / r_mr;
-data.RR_pos = all_data.Shock_Pots_RR_Lin_Pot_Value.Value / r_mr;
+% time window
+t_start = 1480;
+t_end   = 2950;
 
+% logical mask for the time range
+mask = all_data.Shock_Pots_FL_Lin_Pot_Value.Time >= t_start & ...
+       all_data.Shock_Pots_FL_Lin_Pot_Value.Time <= t_end;
+
+% apply to each signal
+data.time  = all_data.Shock_Pots_FL_Lin_Pot_Value.Time(mask);
+data.FL_pos = all_data.Shock_Pots_FL_Lin_Pot_Value.Value(mask) / f_mr;
+data.FR_pos = all_data.Shock_Pots_FR_Lin_Pot_Value.Value(mask) / f_mr;
+data.RL_pos = all_data.Shock_Pots_RL_Lin_Pot_Value.Value(mask) / r_mr;
+data.RR_pos = all_data.Shock_Pots_RR_Lin_Pot_Value.Value(mask) / r_mr;
+
+%%
 % sampling
 dt = mean(diff(data.time));
 Fs = 1/dt; 
@@ -46,7 +55,8 @@ for i = 1:numel(fields)
     subplot(2,2,i)
     
     binWidth = 0.5; % in/s
-    h = histogram(vel,'BinWidth',binWidth,'Normalization','probability');
+    h = histogram(vel,'BinWidth',binWidth,'Normalization','probability', ...
+              'HandleVisibility','off');
     hold on
     
     % stats
@@ -79,8 +89,6 @@ for i = 1:numel(fields)
     xlim([-3*sigma*1.1, 3*sigma*1.1])
     box off
 end
-
-% font
 set(findall(gcf,'-property','FontName'),'FontName','Times New Roman')
 set(findall(gcf,'-property','FontSize'),'FontSize',12)
 
@@ -94,3 +102,43 @@ p_covered = mean(all_vel >= envelope(1) & all_vel <= envelope(2)) * 100;
 
 fprintf('3σ envelope: [%.3f , %.3f] in/s\n', envelope(1), envelope(2));
 fprintf('Coverange within 3σ: %.2f%%\n', p_covered);
+
+% plot enveloped
+
+figure
+enveloped_vel = all_vel(all_vel >= envelope(1) & all_vel <= envelope(2));
+
+binWidth = 0.5; % in/s
+h = histogram(enveloped_vel,'BinWidth',binWidth,'Normalization','probability'...
+    ,'HandleVisibility','off');
+hold on
+
+% stats
+mu_env = mean(enveloped_vel);
+sigma_env = std(enveloped_vel);
+
+% vertical lines
+xline(mu_env,'k--','LineWidth',1.5, ...
+    'DisplayName',sprintf('Mean = %.2f',mu_env));
+
+xline(mu_env+sigma_env,'r--','LineWidth',1.2, ...
+    'DisplayName',sprintf('+1σ = %.2f',mu_env+sigma_env));
+xline(mu_env-sigma_env,'r--','LineWidth',1.2,'HandleVisibility','off');
+
+xline(mu_env+3*sigma_env,'b--','LineWidth',1.2, ...
+    'DisplayName',sprintf('+3σ = %.2f',mu_env+3*sigma_env));
+xline(mu_env-3*sigma_env,'b--','LineWidth',1.2,'HandleVisibility','off');
+
+% format
+xlabel('Wheel Velocity [in/s]');
+ylabel('Percentage [%]')
+ylim([0 max(h.Values)*1.2])
+yt = yticks; 
+yticklabels(yt*100)
+grid on
+legend('Location','NorthWest')
+xlim([envelope(1), envelope(2)])
+box off
+
+set(findall(gcf,'-property','FontName'),'FontName','Times New Roman')
+set(findall(gcf,'-property','FontSize'),'FontSize',12)
