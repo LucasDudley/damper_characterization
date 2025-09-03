79b4f21ce5107f86c01fda55389c699ad68f8d78
LucasDudley <141531242+LucasDudley@users.noreply.github.com>
2025-09-03 15:51:24 -0700

    update dynamic model plots

--- dynamic model/dynamic_torque_calcs.m
+++ dynamic model/dynamic_torque_calcs.m
@@ -3,3 +3,23 @@ clear, clc, close all
 
 %define the input params
 model_params
+
+out = sim("torque_calcs.slx");
+%%
+
+figure
+yyaxis left
+plot( out.kinematics.Data(:,2)*39.37, out.forces.Data(:,1)/4.44)
+ylabel('Dynamic Accel Force [lbf]')
+
+hold on
+yyaxis right
+plot( out.kinematics.Data(:,2)*39.37, out.forces.Data(:,2)/4.44)
+ylabel('Damping Force [lbf]')
+
+box off
+xlabel('Linear Speed [rad/s]')
+legend('Dynamic Acceleration Force', 'Damping Force')
+
+%%
+figure
